19891eff63fb2d42b945319a10cfdc2ec244d7b4
Yurii Dubnytskyi <41127730+YuriiDubnytskyi@users.noreply.github.com>
2019-08-06 22:43:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
-const ServerPortRouter = express.Router();
+const ServerPortRouter = require('./client/routes/ServerPortRouter');
 
 
 
@@ -14,55 +14,7 @@ client.connect(err => {
 });
 
 
-ServerPortRouter.route('/add').post(function (req, res) {
-  /*const serverport = new ServerPort(req.body);
-  serverport.save()
-    .then(serverport => {
-        res.json('Server added successfully');
-    })
-    .catch(err => {
-    res.status(400).send("unable to save to database");
-    });*/
-    client.connect(err => {
-  		const collection = client.db("test").collection("servers");
-  		collection.insertOne(req.body,(err,res)=>{
-    		if (err) throw err;
-        	console.log("1 document inserted");
-    	})
-	});
-    
 
-
-
-});
-
-ServerPortRouter.route('/').get(function (req, res) {
-    /*ServerPort.find(function (err, serverports){
-    if(err){
-      console.log(err);
-    }
-    else {
-      res.json(serverports);
-    }
-  });*/
-	collection.find({}).toArray(function(err, result) {
-    	if (err) {throw err}
-    	else{
-    	console.log(result);
-    	res.json(result)}
-  	});
-  	client.connect(err => {
-  		const collection = client.db("test").collection("servers");
-  		collection.find({}).toArray(function(err, result) {
-    	if (err) {throw err}
-    	else{
-    	console.log(result);
-    	res.json(result)}
-  	});
-	});
-
-
-});
 
 app.use('/serverport', ServerPortRouter);
 // Serve static files from the React app
